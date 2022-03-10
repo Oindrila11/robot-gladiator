4b9db66f1c8433f86c8cd73c069e9e6cbedeaaf1
Oindrila Talukder <oindrilatalukder.ot@gmail.com>
2022-03-09 22:08:46 -0600

    added conditions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,5 @@
 var playerName = window.prompt("What is your robot's name?");
-var playerHealth = 100;
+var playerHealth = 11;
 var playerAttack = 10;
 //you can also log multiple values at once like this 
 console.log(playerName, playerAttack, playerHealth);
@@ -16,11 +16,26 @@ var fight = function() {
  //Log a resulting message to the console so we know that it worked.
  console.log(
      playerName + "attacked" + enemyName + ". " + enemyName + "now has" + enemyHealth + "health remaining."
- )
+ );
+//check enemy's health
+if (enemyHealth <= 0) {
+    window.alert( enemyName  +  "has  died!" );
+}
+else {
+    window.alert ( enemyName +  "still  has"  +  enemyHealth  +  "health  left." )
+} 
 //Substract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable.
-//log a serulting message to the console so we know that it worked.
-
-
+playerHealth = playerHealth-enemyAttack;
+//log a resulting message to the console so we know that it worked.
+console.log(
+     enemyName  +  "attacked"  +  playerName  + ". " +  playerName + "now has" + playerHealth + "health remaining."
+);
+if (playerHealth <=0) {
+    window.alert(playerName + "has died!");
+}
+else {
+    window.alert(playerName + "still has" + playerHealth + "health left");
+}
 
 };
 fight();
